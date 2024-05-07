f78ba0353641cf8d3c29b7c2cf21935ec9ad3fa7
NUCCASJNR <adegbitealareef28@yahoo.com>
2024-05-08 00:01:42 +0100

    updated SQL script

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,8 @@
 -- sql script
 
 
-CREATE DATABASE IF NOT EXISTS SyncWrite_db;
-       CREATE USER IF NOT EXISTS 'Sync_user'@'localhost' IDENTIFIED BY 'Sync_pwd123@';
-              GRANT ALL PRIVILEGES ON SyncWrite_db.* TO 'Sync_user'@'localhost';
-                                      GRANT SELECT ON performance_schema.* TO 'Sync_user'@'localhost';
+CREATE DATABASE IF NOT EXISTS Whisper_db;
+       CREATE USER IF NOT EXISTS 'Whisper_user'@'localhost' IDENTIFIED BY 'Whisper_pwd123@';
+              GRANT ALL PRIVILEGES ON Whisper_db.* TO 'Whisper_user'@'localhost';
+                                      GRANT SELECT ON performance_schema.* TO 'Whisper_user'@'localhost';
 FLUSH PRIVILEGES;
