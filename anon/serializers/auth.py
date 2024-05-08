c287e8cdb539f2690250131853cbf0f36c78a1c2
NUCCASJNR <adegbitealareef28@yahoo.com>
2024-05-08 12:50:52 +0100

    Added signup Serializer

--- anon/serializers/auth.py
+++ anon/serializers/auth.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
 
+"""Contains Authentcation related serializers"""
+
+from rest_framework import serializers
+from anon.models.user import MainUser
+
+
+class SignUpSerializer(serializers.ModelSerializer):
+    """Serializer for handling user signup"""
+    class Meta:
+        model = MainUser
+        fields = ('username', 'password')
