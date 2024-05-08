3cbde4a69ffdc8bd4f2dbbcc6225c1b2b8ffb01b
NUCCASJNR <adegbitealareef28@yahoo.com>
2024-05-08 11:04:21 +0100

    Created message model

---
+++ anon/models/message.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""Message Model For Whisper"""
+from anon.models.user import BaseModel, models, MainUser
+
+
+class Message(BaseModel):
+    """
+    Message Model
+    """
+    sender = models.ForeignKey(MainUser, on_delete=models.CASCADE, related_name='sent_messages')
+    recipient = models.ForeignKey(MainUser, on_delete=models.CASCADE, related_name='received_messages')
+    content = models.TextField()
+    is_read = models.BooleanField(default=True)
+
+    def __str__(self):
+        """
+        String representation of the message.
+        """
+        return f"From: {self.sender.username} | To: {self.recipient.username} | Content: {self.content}"
