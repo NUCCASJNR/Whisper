d571be93b83f85d5bc2ae03c4964ecf0fad16f53
NUCCASJNR <adegbitealareef28@yahoo.com>
2024-05-11 11:44:31 +0100

    Update project

--- anon/serializers/profile.py
+++ anon/serializers/profile.py
@@ -17,7 +17,7 @@ class ProfileSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = MainUser
-        fields = ('username', 'public_key')
+        fields = ('username', 'public_key', 'ready_to_chat')
     
     def get_public_key(self, obj):
         """
@@ -30,3 +30,13 @@ class ProfileSerializer(serializers.ModelSerializer):
             return public_key.get(str(obj.id))
         except ObjectDoesNotExist:
             return 'Not found'
+
+
+class ReadyToChatSerializer(serializers.Serializer):
+    """
+    ReadyToChat Serializer
+    """
+    
+    class Meta:
+        model = MainUser
+        fields = ('ready_to_chat', )
--- anon/views/profile.py
+++ anon/views/profile.py
@@ -4,8 +4,8 @@
 
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
-from anon.serializers.profile import ProfileSerializer
-from rest_framework.response import Response
+from anon.serializers.profile import ProfileSerializer, ReadyToChatSerializer, MainUser
+from rest_framework.response import Response+
 
 
 class ProfileView(APIView):
@@ -22,3 +22,26 @@ class ProfileView(APIView):
         serializer = self.serializer_class(request.user)
         return Response(serializer.data)
 
+
+class ReadyToChatView(APIView):
+    """
+    Ready to chat view
+    """
+    permission_classes = [IsAuthenticated]
+    # serializer_class = ReadyToChatSerializer
+
+    def post(self, request):
+        """
+        Post request for handling ReadyToChat toggle
+        :param request: Request obj
+        :return: None
+        """
+
+        option = request.data.get('Option')
+        current_user = request.user
+        MainUser.custom_update(filter_kwargs={'id': current_user.id},
+                               update_kwargs={'ready_to_chat': option})
+        return Response({
+            'message': 'Status Successfully Updated',
+            'status':
+        })
