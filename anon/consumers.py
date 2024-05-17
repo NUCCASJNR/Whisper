aaf8e2c2522f1104c552c677d8493c8bbbb1ffdc
NUCCASJNR <adegbitealareef28@yahoo.com>
2024-05-17 02:17:57 +0100

    Finally, Websocket works fine!!!

--- anon/consumers.py
+++ anon/consumers.py
@@ -1,3 +1,28 @@
+#!/usr/bin/env python3
+
+
+"""
+MessageConsumer Documentation
+
+This module defines the MessageConsumer class, which is responsible for handling WebSocket
+connections for real-time messaging features in the anon app.It manages authentication,
+ message reception, storage, and broadcasting of messages to connected clients.
+
+Classes:
+    MessageConsumer: Handles WebSocket connections for real-time messaging.
+
+Methods:
+    connect: Establishes a WebSocket connection and processes stored messages.
+    disconnect: Closes the WebSocket connection.
+    receive: Processes incoming messages from clients.
+    chat_message: Sends a message to all clients in the current chat room.
+    get_auth_info: Decodes JWT tokens to retrieve user authentication information.
+    get_user_by_id: Retrieves a user object by ID.
+    save_message: Saves a new message to the database.
+    get_stored_messages: Fetches previously stored messages between two users.
+    process_stored_messages: Sends previously stored messages to the client upon connection.
+"""
+
 import json
 import logging
 from channels.generic.websocket import AsyncWebsocketConsumer
@@ -7,54 +32,55 @@ from django.db.models import Q
 from anon.models.message import MainUser, PlainTextMessage
 from datetime import datetime
 
-
 logging.basicConfig(level=logging.DEBUG, filename='app.log')
 
 
-
 class MessageConsumer(AsyncWebsocketConsumer):
+    """
+    Message Consumer
+    """
+
     async def connect(self):
-        print("Connect method called")
+        """
+        Connect method
+        :return: Nothing
+        """
         self.receiver_id = self.scope['url_route']['kwargs']['receiver_id']
         self.sender_id = self.scope['url_route']['kwargs']['sender_id']
         token = self.scope.get("query_string").decode().split("Bearer%20")[1]
-
-        print(f'Token: {token}')
         auth_info = await self.get_auth_info(token)
-        print(auth_info)
-
         if not auth_info:
             await self.close()
             return
-
         self.user_id = auth_info['user_id']
         self.user = await self.get_user_by_id(auth_info['user_id'])
-        print("user:", self.user)
-        sender_room_name = f"chat_{self.user_id}"
-        receiver_room_name = f"chat_{self.receiver_id}"
         self.room_group_name = f"chat_{self.sender_id}_{self.receiver_id}"
-        print("Room Group Name:", self.room_group_name)
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
         await self.process_stored_messages(self.receiver_id, self.sender_id)
 
     async def disconnect(self, close_code):
-        print("DisConnect method called")
+        """
+        Disconnect method
+        :param close_code: Close code
+        :return:
+        """
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
-        logging.info("User disconnected with ID: %s", self.user_id)
 
     async def receive(self, text_data):
-        print("Receive method called")
-        print(text_data)
+        """
+        Receives incoming messages
+        :param text_data: Incoming message
+        :return: Processed messages
+        """
         try:
             text_data_json = json.loads(text_data)
-            print(text_data_json)
+            print(text_data, text_data_json)
             message = text_data_json["message"]
             sender = text_data_json.get("sender", self.user.username)
             receiver = str(self.scope['url_route']['kwargs']['receiver_id'])
             send = str(text_data_json.get("sender", self.user.id))
             sender_id = await self.get_user_by_id(send)
-            receiver_room_name = f"chat_{receiver}"
             logging.info("Received message '%s' from sender '%s' in room '%s'", message, sender, self.room_group_name)
             response = await self.save_message(message, sender_id, await self.get_user_by_id(receiver))
             obj = datetime.fromisoformat(str(response.updated_at))
@@ -71,11 +97,14 @@ class MessageConsumer(AsyncWebsocketConsumer):
             logging.warning("Received an empty or invalid JSON message")
 
     async def chat_message(self, event):
-        print(f'Event: {event}')
-        print("Chat message method called")
+        """
+        Chat message
+        :param event: 
+        :return:
+        """
+        print('event:', event)
         message = event["message"]
         sender = event["sender"]
-        print(sender)
         logging.info("Sending message '%s' from sender '%s' to room '%s'", message, sender, self.room_group_name)
         await self.send(text_data=json.dumps({
             "message": message,
@@ -84,6 +113,11 @@ class MessageConsumer(AsyncWebsocketConsumer):
         }))
 
     async def get_auth_info(self, token):
+        """
+        Get authentication info from a JWT token
+        :param token: JWT token
+        :return: User ID or False
+        """
         try:
             decoded_token = AccessToken(token)
             print(f'Decoded token: {decoded_token}')
@@ -98,6 +132,11 @@ class MessageConsumer(AsyncWebsocketConsumer):
 
     @sync_to_async
     def get_user_by_id(self, user_id):
+        """
+        Retrieve a user object by ID
+        :param user_id: User ID
+        :return: User object or None
+        """
         try:
             user = MainUser.objects.get(id=user_id)
             return user
@@ -108,6 +147,13 @@ class MessageConsumer(AsyncWebsocketConsumer):
 
     @sync_to_async
     def save_message(self, content, sender, receiver):
+        """
+        Save a new message to the database
+        :param content: Content of the message
+        :param sender: Sender ID
+        :param receiver: Receiver ID
+        :return: Saved message object
+        """
         try:
             message = PlainTextMessage.custom_save(**{'content': content, 'sender': sender, 'recipient': receiver})
             return message
@@ -116,6 +162,12 @@ class MessageConsumer(AsyncWebsocketConsumer):
 
     @sync_to_async
     def get_stored_messages(self, recipient_id, sender_id):
+        """
+        Fetch previously stored messages between two users
+        :param recipient_id: Recipient ID
+        :param sender_id: Sender ID
+        :return: List of message objects
+        """
         try:
             messages = PlainTextMessage.objects.filter(
                 (Q(recipient=recipient_id) & Q(sender=sender_id)) | (Q(recipient=sender_id) & Q(sender=recipient_id))
@@ -126,9 +178,14 @@ class MessageConsumer(AsyncWebsocketConsumer):
             return []
 
     async def process_stored_messages(self, recipient_id, sender_id):
+        """
+        Process and send previously stored messages to the client upon connection
+        :param recipient_id: Recipient ID
+        :param sender_id: Sender ID
+        :return: None
+        """
         messages = await self.get_stored_messages(recipient_id, sender_id)
         for message in messages:
-            # Assuming get_user_by_id is correctly implemented and returns the user object
             sender_user = await self.get_user_by_id(message.sender_id)
             obj = datetime.fromisoformat(str(message.updated_at))
             time = obj.strftime("%A, %d %B %Y, %I:%M %p")
@@ -137,4 +194,3 @@ class MessageConsumer(AsyncWebsocketConsumer):
                 "sender": sender_user.username if sender_user else "Anonymous",
                 "time": time
             }))
-
