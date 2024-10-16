ee6c80fe21b3639e84f7dc3e8a8ada15369a6cd3
NUCCASJNR <adegbitealareef28@yahoo.com>
2024-10-16 02:57:15 +0100

    update setup.py

--- Whisper_cli/setup.py
+++ Whisper_cli/setup.py
@@ -22,8 +22,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "requests>=2.25.1",
-        "argparse",
-        "logging",
+        "argparse"
     ],
     entry_points={
         'console_scripts': [
