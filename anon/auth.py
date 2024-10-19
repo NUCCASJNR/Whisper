6ca793c0ea5d02a185745a13447fe55004217b77
NUCCASJNR <adegbitealareef28@yahoo.com>
2024-10-19 12:12:01 +0100

    updated customjwtauth to retrieve refresh token from header

--- anon/auth.py
+++ anon/auth.py
@@ -1,5 +1,6 @@
 from ninja.security import HttpBearer
 from rest_framework_simplejwt.exceptions import InvalidToken
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from anon.models.token import BlacklistedToken
 import json
 
@@ -12,25 +13,28 @@ class CustomJWTAuth(HttpBearer):
                 access_token = access_token.split("Bearer ")[1]
             else:
                 access_token = None
+            print(f'Request: {request.method}')
             if request.method == 'POST':
                 try:
                     body = json.loads(request.body)
-                    raw_token = body.get('refresh_token')
+                    refresh_token = body.get('refresh_token')
                 except (json.JSONDecodeError, TypeError):
-                    raw_token = None
+                    refresh_token = None
             else:
-                raw_token = request.GET.get('refresh_token')
-
-            # Check if the tokens are blacklisted
+                refresh_token = request.headers.get("Authorization")
+                if refresh_token and refresh_token.startswith("Bearer "):
+                    refresh_token = refresh_token.split("Bearer ")[1]
+                else:
+                    refresh_token = None
             access_found = BlacklistedToken.objects.filter(access_token=access_token).exists()
-            refresh_found = BlacklistedToken.objects.filter(refresh_token=raw_token).exists()
+            refresh_found = BlacklistedToken.objects.filter(refresh_token=refresh_token).exists()
 
             if access_found or refresh_found:
-                raise InvalidToken({"detail": "This token has been blacklisted.", "code": "token_not_valid"})
-            
-            # Call the parent method to complete authentication
-            return super().authenticate(request, token)
+                return None
+            jwt_auth = JWTAuthentication()
+            validated_token = jwt_auth.get_validated_token(token)
+            user = jwt_auth.get_user(validated_token)
+            return user
 
-        except InvalidToken as e:
-            # Only catch and re-raise the exception with additional context if necessary
-            raise InvalidToken({"detail": "Invalid or blacklisted token", "code": "token_not_valid"}) from e
+        except InvalidToken:
+            return None
